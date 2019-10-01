0eaaeecd3950a12942839d85bbf1710ea3711bf8
zyrave <rob.rivermark@gmail.com>
2019-10-01 15:32:50 +0700

    Setup Apollo

---
+++ pages/_app.tsx
@@ -0,0 +1,19 @@
+import App from 'next/app';
+import React from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+import withApollo from '../lib/withApollo';
+
+class MyApp extends App<any> {
+  render() {
+    const { Component, pageProps, apolloClient } = this.props;
+
+    return (
+      <ApolloProvider client={apolloClient}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    );
+  }
+}
+
+export default withApollo(MyApp);
