885b107296371558e6fab2de630d99c1193c8d4b
zyrave <rob.rivermark@gmail.com>
2019-10-02 10:39:08 +0700

    Fix favicon.ico not found

---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, { Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <html lang="id">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <link rel="icon" type="image/x-icon" href="../static/favicon.ico" />
+        </Head>
+        <body className="custom_class">
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
