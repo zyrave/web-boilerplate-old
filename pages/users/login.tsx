14bf41c4b681614d486233ef8ae98b996b3751cd
zyrave <rob.rivermark@gmail.com>
2020-02-11 16:46:34 +0700

    Add alert for invalid login

--- pages/users/login.tsx
+++ pages/users/login.tsx
@@ -3,13 +3,19 @@ import { NextPage } from 'next';
 import Link from 'next/link';
 import Router from 'next/router';
 import { NextSeo } from 'next-seo';
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Card, Col, Container, Form, Row } from 'react-bootstrap';
 
-import { InputField } from '../../modules/shared/fields/InputField';
 import { useLoginMutation, MeDocument } from '../../generated/graphql';
+import { InputField } from '../../modules/shared/fields/InputField';
+import { Alert } from '../../modules/shared';
 
 const Login: NextPage = () => {
+  const [alert, setAlert] = useState<any>({
+    show: false,
+    type: 'success',
+    message: '',
+  });
   const [login] = useLoginMutation();
 
   return (
@@ -18,29 +24,25 @@ const Login: NextPage = () => {
       <div className="app flex-row align-items-center">
         <Container>
           <Row className="justify-content-center">
-            <Col md="6" lg="4">
+            <Col sm="8" md="6" lg="4">
               <Card className="p-4">
                 <Card.Body>
                   <h1 className="text-center">Login</h1>
                   <p className="text-center text-muted">Sign In to your account</p>
-                  {/* <Alert color="danger" isOpen={!!errorMessage} className="text-center">
-                      {errorMessage}
-                    </Alert> */}
+                  <Alert
+                    show={alert.show}
+                    type={alert.type}
+                    message={alert.message}
+                    onClose={() => setAlert({ show: false })}
+                  />
                   <Formik
                     initialValues={{
                       email: '',
                       password: '',
                     }}
                     onSubmit={async (data, { setErrors }) => {
-                      // if (data.password !== data.confirmPassword) {
-                      //   setShowAlert(true);
-                      //   return;
-                      // }
-
-                      // setShowAlert(false);
-
                       try {
-                        await login({
+                        const response = await login({
                           variables: data,
                           update: (cache, { data: d }) => {
                             if (!d || !d.login) {
@@ -53,14 +55,16 @@ const Login: NextPage = () => {
                           },
                         });
 
-                        // if (response && response.data && !response.data.login) {
-                        //   setErrors({
-                        //     email: 'Invalid login',
-                        //   });
-                        //   return;
-                        // }
+                        if (response && response.data && !response.data.login) {
+                          setAlert({
+                            show: true,
+                            type: 'danger',
+                            message: 'Invalid login',
+                          });
+                        }
                         Router.push('/');
                       } catch (err) {
+                        console.log('TRYC:', err);
                         const errors: { [key: string]: string } = {};
 
                         err.graphQLErrors[0].extensions.exception.validationErrors.map((validationErr: any) => {
